6eb137097b2a9729f46d50a854a4843f9c005acd
Raabidus <kralmartin93@gmail.com>
2024-07-26 09:27:05 +0200

    init

--- gui.py
+++ gui.py
@@ -1,1 +1,15 @@
-# Create GUI using Tkinter
+import tkinter as tk
+from tkinter import ttk
+
+
+# window
+window = tk.Tk()
+window.title('Importer test gui')
+window.geometry('500x350')
+
+# title
+title_label = ttk.Label(master = window, text = 'test')
+title_label.pack()
+
+# run window
+window.mainloop()
